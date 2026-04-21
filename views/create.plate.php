768f4de33af2d7c866e78c11e934d80719869bc5
Leo Nickl <leo@leonickl.de>
2026-04-21 09:04:19 +0200

    show file picker warning

--- views/create.plate.php
+++ views/create.plate.php
@@ -23,6 +23,11 @@
         </div>
     </div>
 
+    <p class="mt">Das Bild möglichst nicht mit dem Google-Foto-Picker,
+        sondern am besten <span class="highlight">über "Dateien" oder so hochladen</span>, sonst
+        kann es sein, dass die Koordinaten zurückgesetzt werden.
+        Oder am Laptop, da sollte es immer klappen.</p>
+
     <div class="row mt end">
         <a href="/" class="btn">Zurück</a>
         <input type="submit" class="btn" value="Speichern" />
